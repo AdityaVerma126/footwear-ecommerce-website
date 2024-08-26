dea06c4a4d4f6e71f0d614c2d8a25b213d223743
aditya verma <adityaverma12609@gmail.com>
2024-08-26 10:36:22 +0530

    this is initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+
+
+  <h2 align="center">Footcap - eCommerce website</h2>
+
+  Footcap is fully responsive ecommerce website, <br />Responsive for all devices, built using HTML, CSS, and JavaScript.
+
+  <a href="https://codewithsadee.github.io/footcap/"><strong>➥ Live Demo</strong></a>
+
+</div>
+
+<br />
+
+### Demo Screeshots
+
+![Footcap Desktop Demo](./readme-images/desktop.png "Desktop Demo")
+
+### Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+
+
+### Contact
+
+If you want to contact with me you can reach me at ADITYA VERMA
+
+
---
+++ server.js
@@ -0,0 +1,39 @@
+
+const express=require("express")
+const bodyParser = require("body-parser")
+
+const app = express();
+
+
+app.use(express.static("assets"))
+app.use(bodyParser.urlencoded({extended:true}))
+app.get("/",(req,res)=>{
+    res.sendFile( __dirname +"/index.html")
+
+});
+app.post("/",(req,res)=>{
+  
+    var email = req.body.email;
+    console.log( email);
+    res.sendFile(__dirname+"/sucess.html")
+
+
+})
+app.get("/about",(req,res)=>{
+    res.sendFile(__dirname+"/about.html")
+})
+app.get("/blog",(req,res)=>{
+    res.sendFile(__dirname+"/blog.html")
+})
+app.post("/s",(req,res)=>{
+    var user= req.body.username;
+    var pword = req.body.password;
+    console.log(user, pword);
+    res.redirect("/")
+  
+})
+
+    
+app.listen(3000,function(){
+    console.log("server started")
+})
